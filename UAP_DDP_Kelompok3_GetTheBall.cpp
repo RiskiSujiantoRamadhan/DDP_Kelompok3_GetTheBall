d84bbea114010e1263c1bd01190db048de09076b
Riski Sujianto Ramadhan <83487523+RiskiSujiantoRamadhan@users.noreply.github.com>
2023-12-11 19:16:21 +0700

    Update UAP_DDP_Kelompok3_GetTheBall.cpp
    Optimasi tampilan, menambahkan warna, dan menambahkan bola menjadi 2

--- UAP_DDP_Kelompok3_GetTheBall.cpp
+++ UAP_DDP_Kelompok3_GetTheBall.cpp
@@ -2,40 +2,43 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
 
-// Fungsi untuk menggambar bola pada posisi tertentu
+// Fungsi untuk menggambar bola dan warna
 void drawBall(int x, int y) {
-    mvprintw(y, x, "0");
+    attron(COLOR_PAIR(1) | A_BOLD); // atribut warna dengan pair 1 dan tebal
+    mvprintw(y, x, "O");
 }
-
-// Fungsi untuk menggambar persegi panjang pada posisi tertentu
+// Fungsi untuk menggambar persegi panjang dan warna biru muda dan tebal
 void drawRectangle(int x, int y, int length) {
+    attron(COLOR_PAIR(1) | A_BOLD); // atribut warna dengan pair 1 dan tebal
     for (int i = 0; i < length; ++i) {
         mvprintw(y, x + i, "=");
     }
+    attroff(COLOR_PAIR(1) | A_BOLD); // Menonaktifkan atribut warna dan tebal
 }
 
 // Fungsi untuk menampilkan menu start
-void showCreativeStartMenu() {
-    mvprintw(5, 10, "*****************************");
-    mvprintw(6, 10, "*     WELCOME TO Get My Ball    *");
-    mvprintw(7, 10, "*                           *");
-    mvprintw(8, 10, "*        PRESS '1' TO       *");
-    mvprintw(9, 10, "*        START THE GAME     *");
-    mvprintw(10, 10, "*                           *");
-    mvprintw(11, 10, "*        PRESS '2' TO       *");
-    mvprintw(12, 10, "*           EXIT            *");
-    mvprintw(13, 10, "*                           *");
-    mvprintw(14, 10, "*****************************");
+void showStartMenu() {
+    mvprintw(8, 35, "*************************************************");
+    mvprintw(9, 35, "*                                               *");
+    mvprintw(10, 35, "*             WELCOME TO GET MY BALL            *");
+    mvprintw(11, 35, "*                                               *");
+    mvprintw(12, 35, "*                  PRESS '1' TO                 *");
+    mvprintw(13, 35, "*                 START THE GAME                *");
+    mvprintw(14, 35, "*                                               *");
+    mvprintw(15, 35, "*                 PRESS '2' TO                  *");
+    mvprintw(16, 35, "*                     EXIT                      *");
+    mvprintw(17, 35, "*                                               *");
+    mvprintw(18, 35, "*************************************************");
     refresh();
 }
 
 int main() {
     // Inisialisasi ncurses
     initscr();
-    // Menyembunyikan kursor
+    start_color(); // Mengaktifkan mode warna
+    init_pair(1, COLOR_CYAN, COLOR_BLACK);
     curs_set(0);
     // Mendisable buffering agar karakter langsung tercetak
     nodelay(stdscr, TRUE);
@@ -51,31 +54,39 @@ int main() {
 
     int rectX = maxX / 2; // Posisi awal persegi panjang
     int rectY = maxY - 3; // Posisi awal lebih ke atas 2 baris
-    int rectLength = 10;  // Panjang awal persegi panjang
-    int rectSpeed = 5;    // Kecepatan pergerakan persegi panjang
+    int rectLength = 8;   // Panjang awal persegi panjang
+    int rectSpeed = 6;    // Kecepatan pergerakan persegi panjang
     int rectSpeedX = 0;   // Kecepatan pergerakan horizontal
     int rectSpeedY = 0;   // Kecepatan pergerakan vertikal
 
-    int ballX = rand() % maxX; // Posisi awal bola
-    int ballY = 0;
+    int ballX1 = rand() % maxX; // Posisi awal bola 1/random(acak)
+    int ballY1 = 0;
+
+    int ballX2 = rand() % maxX; // Posisi awal bola 2/random(acak)
+    int ballY2 = -15; // Memberikan jarak supaya bola kedua tidak jatuh bersamaan dengan bola pertama
 
     int ch;
-    int speed = 150; // Kecepatan jatuh bola, semakin kecil semakin cepat
+    int speed = 180; // Kecepatan jatuh bola
 
     int score = 0; // Skor awal
+    long elapsedTime = 0; // Waktu yang diperlukan
+
+    int minutes, seconds; // variabel waktu
 
     bool game_over = false;
     bool start_menu = true;
 
+    time_t startTime = time(nullptr); // Inisialisasi waktu awal
+
     while (start_menu) {
-        showCreativeStartMenu();
+        showStartMenu();
         ch = getch();
         switch (ch) {
             case '1':
                 start_menu = false;
                 break;
             case '2':
-                endwin(); // Menutup ncurses sebelum keluar
+                endwin();
                 return 0;
             default:
                 break;
@@ -85,26 +96,56 @@ int main() {
     while (!game_over) {
         clear(); // Membersihkan layar
 
-        // Menggambar bola pada posisi baru
-        drawBall(ballX, ballY);
-        ballY++;
+        // Menggambar bola 1 pada posisi baru dengan warna biru muda dan tebal
+        drawBall(ballX1, ballY1);
+        ballY1++;
 
-        // Menggambar persegi panjang pada posisi baru
+        // Menggambar bola 2 pada posisi baru dengan warna biru muda dan tebal
+        drawBall(ballX2, ballY2);
+        ballY2++;
+
+        // Menggambar persegi panjang pada posisi baru dengan warna biru muda dan tebal
         drawRectangle(rectX, rectY, rectLength);
 
-        // Jika bola mencapai batas bawah, game over
-        if (ballY >= maxY) {
+        // Jika bola 1 mencapai batas bawah maka game over
+        if (ballY1 >= maxY) {
             game_over = true;
         }
 
-        // Jika bola menyentuh persegi panjang, tambahkan skor dan reset bola
-        if (ballY == rectY && ballX >= rectX && ballX < rectX + rectLength) {
+        // Jika bola 1 menyentuh persegi panjang maka mendapat skor dan reset bola
+        if (ballY1 == rectY && ballX1 >= rectX && ballX1 < rectX + rectLength) {
             score++;
-            ballX = rand() % maxX;
-            ballY = 0;
+            ballX1 = rand() % maxX;
+            ballY1 = 0;
         }
-// Menampilkan skor real-time di pojok kiri atas
+
+        // Jika bola 2 mencapai batas bawah maka game over
+        if (ballY2 >= maxY) {
+            game_over = true;
+        }
+
+        // Jika bola 2 menyentuh persegi panjang maka mendapat skor dan reset bola
+        if (ballY2 == rectY && ballX2 >= rectX && ballX2 < rectX + rectLength) {
+            score++;
+            ballX2 = rand() % maxX;
+            ballY2 = 0;
+        }
+
+        // Menampilkan skor real-time di pojok kiri atas
         mvprintw(0, 0, "Skor: %d", score);
+
+        // Menampilkan waktu di bawah skor
+        time_t currentTime = time(nullptr);
+        elapsedTime = difftime(currentTime, startTime);
+
+        // Handle overflow
+        if (elapsedTime < 0) {
+            elapsedTime = 0;
+        }
+
+        minutes = elapsedTime / 60;
+        seconds = elapsedTime % 60;
+        mvprintw(1, 0, "Waktu: %02d:%02d", minutes, seconds);
 
         // Memperbarui tampilan
         refresh();
@@ -112,7 +153,7 @@ int main() {
         // Mengatur kecepatan jatuh bola
         napms(speed);
 
-        // Menangani input dari pengguna untuk menggerakkan persegi panjang
+        // Kontroller untuk menggerakkan persegi panjang
         int input = getch();
         switch (input) {
             case KEY_LEFT:
@@ -139,20 +180,20 @@ int main() {
 
     // Membersihkan layar
     clear();
-    // Menampilkan skor akhir dan pesan "Game Over"
-    mvprintw(10, 10, "*****************************");
-    mvprintw(11, 10, "*         GAME OVER         *");
-    mvprintw(12, 10, "*      YOUR SCORE: %d      *", score);
-    mvprintw(13, 10, "*                           *");
-    mvprintw(14, 10, "*  PRESS 'ESC' TO EXIT      *");
-    mvprintw(15, 10, "*****************************");
+    // Menampilkan skor akhir dan waktu 
+    mvprintw(8, 35, "*************************************************");
+    mvprintw(9, 35, "*                                               *");
+    mvprintw(10, 35, "*                   GAME OVER                   *");
+    mvprintw(11, 35, "*                YOUR SCORE: %d                  *", score);
+    mvprintw(12, 35, "*                YOUR TIME: %02d:%02d               *", minutes, seconds);
+    mvprintw(13, 35, "*                                               *");
+    mvprintw(14, 35, "*               PRESS 'ESC' TO EXIT             *");
+    mvprintw(15, 35, "*                                               *");
+    mvprintw(16, 35, "*************************************************");
     refresh();
 
     // Menunggu sampai pengguna menekan tombol ESC
     while (getch() != 27);
-
-    // Menutup ncurses
     endwin();
-
     return 0;
 }
